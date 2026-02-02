a18980a5b8f5f0660c9cce372d2d2e4991d21027
ImRkofficial <rkbagra0490@gmail.com>
2026-02-02 18:33:44 +0530

    SYSCALL: Added close function

--- src/kvm/system.rs
+++ src/kvm/system.rs
@@ -1,7 +1,7 @@
 use std::ffi::{CString};
 use std::io::Error;
 use std::os::fd::RawFd;
-use libc::{open,O_RDWR};
+use libc::{c_int, close, open, O_RDWR};
 use crate::errors::VshError;
 pub struct Kvm {
     fd:RawFd
@@ -22,6 +22,26 @@ impl Kvm {
         Ok(Kvm{
             fd
         })
+    }
+
+    pub fn close(&mut self) -> Result<(),VshError> {
+        let fd:RawFd = self.fd();
+
+        if fd < 0 {
+            // Already Closed
+            return Ok(());
+        }
+        let close_status_number:c_int = unsafe { close(fd)};
+
+        if close_status_number < 0 {
+            return Err(VshError::OsError {
+                syscall: "close",
+                source:Error::last_os_error()
+            });
+        };
+
+        self.fd = -1;
+        return Ok(());
     }
 
     pub fn fd(&self) -> RawFd {
